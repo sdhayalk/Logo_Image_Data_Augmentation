696e6cffaad2d2dee572e546cf04cfc2ef7cc4bc
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-01-31 04:11:28 -0700

    random number of logos at different positions implemented

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class LogoImageManipulation:
 		return resized_image
 
 	def random_rotate(self, image, max_rotation_degree=30):
-		angle = randint(0, max_rotation_degree)
+		angle = randint(-max_rotation_degree, max_rotation_degree)
 
 		'''
 		this logic has been directly referred from Remi Cuingnet's answer here at: https://stackoverflow.com/questions/22041699/rotate-an-image-without-cropping-in-opencv-in-c/37347070#37347070
@@ -68,7 +68,8 @@ class OverlayLogoOnBackground(BackgroundImageManipulation, LogoImageManipulation
 		Returns:
 			numpy array -- overlayed background image with overlay image
 		'''
-		x_offset=y_offset=1
+		x_offset = randint(1, l_img.shape[1] - s_img.shape[1])
+		y_offset = randint(1, l_img.shape[0] - s_img.shape[0])
 		y1, y2 = y_offset, y_offset + s_img.shape[0]
 		x1, x2 = x_offset, x_offset + s_img.shape[1]
 
@@ -81,24 +82,26 @@ class OverlayLogoOnBackground(BackgroundImageManipulation, LogoImageManipulation
 
 		return l_img
 
-	def overlay(self, logo_image_path, background_image_path):
-		self.background_image = cv2.imread(background_image_path)
-		self.logo_image = cv2.imread(logo_image_path, -1)
+	def overlay(self, logo_image, background_image):
+		self.background_image = background_image
+		self.logo_image = logo_image
 
 		self.logo_image = self.random_resize(self.logo_image, max_length=300)	# random resize of logo
 		self.logo_image = self.random_rotate(self.logo_image)
-		cv2.imshow('l',  self.logo_image)
-		cv2.waitKey(0)
 		# self.logo_image = self.random_position(self.logo_image, self.background_image.shape[1] - self.logo_image.shape[1])
 
 		self.overlayed_images = self.blend_transparent(self.background_image, self.logo_image)
-		OverlayLogoOnBackground.counter += 1
 
 		return self.background_image
 
-	def overlay_and_write_to_disk(self, logo_image_path, background_image_path, write_path, file_name):
-		overlayed_image = self.overlay(logo_image_path, background_image_path)
+	def overlay_and_write_to_disk(self, logo_image, background_image, write_path, file_name):
+		OverlayLogoOnBackground.counter += 1
+		overlayed_image = self.overlay(logo_image, background_image)
 		cv2.imwrite(write_path + os.sep + file_name, overlayed_image)
+
+	def write_to_disk(self, overlayed_image, write_path, file_name):
+		OverlayLogoOnBackground.counter += 1
+		cv2.imwrite(write_path + os.sep + file_name, overlayed_image)		
 
 
 def main():
@@ -118,16 +121,16 @@ def main():
 	overlay_generator = OverlayLogoOnBackground()
 	
 	for background_image_file_name in background_images_list:
-		for logo_image_file_name in logo_images_list:
-			logo_image = LOGO_IMAGE_PATH + os.sep + logo_image_file_name
-			background_image = BACKGROUND_IMAGE_PATH + os.sep + background_image_file_name
+		background_image = cv2.imread(BACKGROUND_IMAGE_PATH+os.sep+background_image_file_name)
+		temp_number_of_logos = randint(1, 3)
 
-			# background_image = overlay_generator.resize_background_image(background_image, (DIM_1, DIM_2))
+		for _ in range(temp_number_of_logos):
+			logo_image_file_name = random.choice(logo_images_list)
+			logo_image = cv2.imread(LOGO_IMAGE_PATH+os.sep+logo_image_file_name, -1)
 
-			overlay_generator.overlay_and_write_to_disk(logo_image, \
-														background_image, \
-														OVERLAYED_WRITE_PATH, \
-														background_image_file_name[0:-4] + str(OverlayLogoOnBackground.counter) + '.jpg')
+			background_image = overlay_generator.overlay(logo_image, background_image)
+
+		overlay_generator.write_to_disk(background_image, OVERLAYED_WRITE_PATH, background_image_file_name[0:-4] + str(OverlayLogoOnBackground.counter) + '.jpg')
 
 
 if __name__ == '__main__':
