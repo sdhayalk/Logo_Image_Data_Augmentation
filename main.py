8f285b8c168bf090a653c4897fa3a0e76c768012
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-02-01 19:16:06 -0700

    mc

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class OverlayLogoOnBackground(BackgroundImageManipulation, LogoImageManipulation
 		    l_img[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] +
 		                              alpha_l * l_img[y1:y2, x1:x2, c])
 
-		return l_img
+		return l_img, x1, y1, x2, y2
 
 	def overlay(self, logo_image, background_image):
 		self.background_image = background_image
@@ -88,26 +88,29 @@ class OverlayLogoOnBackground(BackgroundImageManipulation, LogoImageManipulation
 
 		self.logo_image = self.random_resize(self.logo_image, max_length=300)	# random resize of logo
 		self.logo_image = self.random_rotate(self.logo_image)
-		# self.logo_image = self.random_position(self.logo_image, self.background_image.shape[1] - self.logo_image.shape[1])
 
-		self.overlayed_images = self.blend_transparent(self.background_image, self.logo_image)
+		self.overlayed_images, x_min, y_min, x_max, y_max = self.blend_transparent(self.background_image, self.logo_image)
 
-		return self.background_image
+		return self.background_image, x_min, y_min, x_max, y_max
 
 	def overlay_and_write_to_disk(self, logo_image, background_image, write_path, file_name):
 		OverlayLogoOnBackground.counter += 1
-		overlayed_image = self.overlay(logo_image, background_image)
+		overlayed_image, x_min, y_min, x_max, y_max = self.overlay(logo_image, background_image)
 		cv2.imwrite(write_path + os.sep + file_name, overlayed_image)
 
-	def write_to_disk(self, overlayed_image, write_path, file_name):
+	def write_image_to_disk(self, overlayed_image, write_path, file_name):
 		OverlayLogoOnBackground.counter += 1
-		cv2.imwrite(write_path + os.sep + file_name, overlayed_image)		
+		cv2.imwrite(write_path + os.sep + file_name, overlayed_image)
+
+	def write_label_data_to_disk(self, overlayed_image_filename, write_path, class_value, x_min, y_min, x_max, y_max):
+		
+
 
 
 def main():
-	BACKGROUND_IMAGE_PATH = 'G:/DL/Logo_Image_Data_Augmentation/Images/background_images'
-	LOGO_IMAGE_PATH = 'G:/DL/Logo_Image_Data_Augmentation/Images/logo_images'
-	OVERLAYED_WRITE_PATH = 'G:/DL/Logo_Image_Data_Augmentation/Images/overlayed_images'
+	BACKGROUND_IMAGE_PATH = 'G:/DL/data_logo/coco_data/train2017'
+	LOGO_IMAGE_PATH = 'G:/DL/data_logo/coco_data/logo_images'
+	OVERLAYED_WRITE_PATH = 'G:/DL/data_logo/coco_data/overlayed_images'
 	DIM_1 = 256
 	DIM_2 = 256
 	
@@ -120,7 +123,7 @@ def main():
 
 	overlay_generator = OverlayLogoOnBackground()
 	
-	for background_image_file_name in background_images_list:
+	for background_image_file_name in background_images_list[0:50]:
 		background_image = cv2.imread(BACKGROUND_IMAGE_PATH+os.sep+background_image_file_name)
 		temp_number_of_logos = randint(1, 3)
 
@@ -128,9 +131,9 @@ def main():
 			logo_image_file_name = random.choice(logo_images_list)
 			logo_image = cv2.imread(LOGO_IMAGE_PATH+os.sep+logo_image_file_name, -1)
 
-			background_image = overlay_generator.overlay(logo_image, background_image)
+			background_image, x_min, y_min, x_max, y_max = overlay_generator.overlay(logo_image, background_image)
 
-		overlay_generator.write_to_disk(background_image, OVERLAYED_WRITE_PATH, background_image_file_name[0:-4] + str(OverlayLogoOnBackground.counter) + '.jpg')
+		overlay_generator.write_image_to_disk(background_image, OVERLAYED_WRITE_PATH, background_image_file_name[0:-4] + str(OverlayLogoOnBackground.counter) + '.jpg')
 
 
 if __name__ == '__main__':
